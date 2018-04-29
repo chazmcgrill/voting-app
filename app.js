ef328ebbbd0d8e6fe8f7710b7f0da6eb345ac4ea
chazmcgrill <charlietaylordev@gmail.com>
2018-04-29 17:23:24 +0100

    Connected Test MongoDB

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
 // Retrieve
-var MongoClient = require('mongodb').MongoClient;
+const db = 'mongodb://localhost:27017/testdb';
 
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function (err, db) {
-  if (!err) {
-    console.log("We are connected");
-  }
-  db.collection('test', function (err, collection) { });
-});
+mongoose.connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port: ${port}`));
+
